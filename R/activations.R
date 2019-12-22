50a7223979c5a1860c16abab786c9cb5f44776d9
Greg McMahan <gmcmacran@gmail.com>
2019-12-22 12:32:21 -0600

    Adding classification tests

--- R/activations.R
+++ R/activations.R
@@ -1,5 +1,5 @@
 elu_R <- function(x, alpha = 1) {
-  out <- ifelse (x >0,
+  out <- ifelse(x > 0,
     x,
     alpha * (exp(x) - 1)
   )
@@ -14,50 +14,50 @@ softmax_R <- function(x) {
   return(out)
 }
 
-softplus_R <- function(x){
+softplus_R <- function(x) {
   out <- log(exp(x) + 1)
 
   return(out)
 }
 
-softsign_R <- function(x){
+softsign_R <- function(x) {
   out <- x / (abs(x) + 1)
 
   return(out)
 }
 
 relu_R <- function(x, alpha = 0, max_value = Inf, threshold = 0) {
-  out <- ifelse(x >= threshold, x, alpha * (x-threshold))
+  out <- ifelse(x >= threshold, x, alpha * (x - threshold))
   out <- pmin(out, max_value)
 
   return(out)
 }
 
-tanh_R <- function(x){
+tanh_R <- function(x) {
   out <- tanh(x)
 
   return(out)
 }
 
-sigmoid_R <- function(x){
+sigmoid_R <- function(x) {
   out <- 1 / (1 + exp(-x))
 
   return(out)
 }
 
-hard_sigmoid_R <- function(x){
-  out <- ifelse(x < -2.5, 0, ifelse(x > 2.5, 1, .2*x + .5))
+hard_sigmoid_R <- function(x) {
+  out <- ifelse(x < -2.5, 0, ifelse(x > 2.5, 1, .2 * x + .5))
 
   return(out)
 }
 
-exponential_R <- function(x){
+exponential_R <- function(x) {
   out <- exp(x)
 
   return(out)
 }
 
-linear_R <- function(x){
+linear_R <- function(x) {
   out <- identity(x)
 
   return(out)
--- tests/testthat/test C simulate_classification_data.R
+++ tests/testthat/test C simulate_classification_data.R
@@ -1,4 +1,4 @@
-context("Testing simulate_regression_data")
+context("Testing simulate_classification_data")
 
 ###############################################
 # Run code
@@ -6,28 +6,28 @@ context("Testing simulate_regression_data")
 set.seed(1)
 
 ###################
-# Linear Regression
+# logistic Regression
 ###################
 
-matrices <- list(matrix(10, nrow = 1, ncol = 1))
-activations <- list(linear_R)
+matrices <- list(matrix(.1, nrow = 1, ncol = 1))
+activations <- list(sigmoid_R)
 
-out <- simulate_regression_data(rows = 100L, N = 1L, U = 0L, C = 0L, matrices = matrices, activations = activations, noise = 0)
+out <- simulate_classification_data(rows = 100L, N = 1L, U = 0L, C = 0L, matrices = matrices, activations = activations)
 
 test_that("Linear Regression 1 variable", {
   expect_true(ncol(out) == 2)
-  expect_true(all(round(out[, 1] * 10, 8) == round(out[, 2], 8)))
+  expect_true(all(out[, 2] %in% c(0, 1)))
 })
 
 
 matrices <- list(matrix(1:3, nrow = 3, ncol = 1))
-activations <- list(linear_R)
+activations <- list(sigmoid_R)
 
-out <- simulate_regression_data(rows = 100L, N = 3L, U = 0L, C = 0L, matrices = matrices, activations = activations, noise = 0)
+out <- simulate_classification_data(rows = 100L, N = 3L, U = 0L, C = 0L, matrices = matrices, activations = activations)
 
 test_that("Linear Regression 3 variables", {
   expect_true(ncol(out) == 4)
-  expect_true(all(round(out[, 1] * 1 + out[, 2] * 2 + out[, 3] * 3, 8) == round(out[, 4], 8)))
+  expect_true(all(out[, 4] %in% c(0, 1)))
 })
 
 rm(matrices, activations)
@@ -37,9 +37,9 @@ rm(matrices, activations)
 ###################
 numVar <- 1L
 matrices <- list(matrix(1:numVar, nrow = numVar, ncol = 1))
-activations <- list(linear_R)
+activations <- list(sigmoid_R)
 
-out <- simulate_regression_data(rows = 10L, N = numVar, U = 0L, C = 0L, matrices = matrices, activations = activations, noise = 0)
+out <- simulate_classification_data(rows = 10L, N = numVar, U = 0L, C = 0L, matrices = matrices, activations = activations)
 
 test_that("1 normal variable", {
   expect_true(ncol(out) == numVar + 1)
@@ -47,9 +47,9 @@ test_that("1 normal variable", {
 
 numVar <- 3L
 matrices <- list(matrix(1:numVar, nrow = numVar, ncol = 1))
-activations <- list(linear_R)
+activations <- list(sigmoid_R)
 
-out <- simulate_regression_data(rows = 10L, N = numVar, U = 0L, C = 0L, matrices = matrices, activations = activations, noise = 0)
+out <- simulate_classification_data(rows = 10L, N = numVar, U = 0L, C = 0L, matrices = matrices, activations = activations)
 
 test_that("1 normal variable", {
   expect_true(ncol(out) == numVar + 1)
@@ -57,9 +57,9 @@ test_that("1 normal variable", {
 
 numVar <- 5L
 matrices <- list(matrix(1:numVar, nrow = numVar, ncol = 1))
-activations <- list(linear_R)
+activations <- list(sigmoid_R)
 
-out <- simulate_regression_data(rows = 10L, N = numVar, U = 0L, C = 0L, matrices = matrices, activations = activations, noise = 0)
+out <- simulate_classification_data(rows = 10L, N = numVar, U = 0L, C = 0L, matrices = matrices, activations = activations)
 
 test_that("1 normal variable", {
   expect_true(ncol(out) == numVar + 1)
@@ -72,9 +72,9 @@ rm(numVar, matrices, activations)
 ###################
 numVar <- 1L
 matrices <- list(matrix(1:numVar, nrow = numVar, ncol = 1))
-activations <- list(linear_R)
+activations <- list(sigmoid_R)
 
-out <- simulate_regression_data(rows = 10L, N = 0L, U = numVar, C = 0L, matrices = matrices, activations = activations, noise = 0)
+out <- simulate_classification_data(rows = 10L, N = 0L, U = numVar, C = 0L, matrices = matrices, activations = activations)
 
 test_that("1 normal variable", {
   expect_true(ncol(out) == numVar + 1)
@@ -82,9 +82,9 @@ test_that("1 normal variable", {
 
 numVar <- 3L
 matrices <- list(matrix(1:numVar, nrow = numVar, ncol = 1))
-activations <- list(linear_R)
+activations <- list(sigmoid_R)
 
-out <- simulate_regression_data(rows = 10L, N = 0L, U = numVar, C = 0L, matrices = matrices, activations = activations, noise = 0)
+out <- simulate_classification_data(rows = 10L, N = 0L, U = numVar, C = 0L, matrices = matrices, activations = activations)
 
 test_that("1 normal variable", {
   expect_true(ncol(out) == numVar + 1)
@@ -92,9 +92,9 @@ test_that("1 normal variable", {
 
 numVar <- 5L
 matrices <- list(matrix(1:numVar, nrow = numVar, ncol = 1))
-activations <- list(linear_R)
+activations <- list(sigmoid_R)
 
-out <- simulate_regression_data(rows = 10L, N = 0L, U = numVar, C = 0L, matrices = matrices, activations = activations, noise = 0)
+out <- simulate_classification_data(rows = 10L, N = 0L, U = numVar, C = 0L, matrices = matrices, activations = activations)
 
 test_that("1 normal variable", {
   expect_true(ncol(out) == numVar + 1)
@@ -107,9 +107,9 @@ rm(numVar, matrices, activations)
 ###################
 numVar <- 1L
 matrices <- list(matrix(1:numVar, nrow = numVar, ncol = 1))
-activations <- list(linear_R)
+activations <- list(sigmoid_R)
 
-out <- simulate_regression_data(rows = 10L, N = 0L, U = 0L, C = numVar, matrices = matrices, activations = activations, noise = 0)
+out <- simulate_classification_data(rows = 10L, N = 0L, U = 0L, C = numVar, matrices = matrices, activations = activations)
 
 test_that("1 normal variable", {
   expect_true(ncol(out) == numVar + 1)
@@ -117,9 +117,9 @@ test_that("1 normal variable", {
 
 numVar <- 3L
 matrices <- list(matrix(1:numVar, nrow = numVar, ncol = 1))
-activations <- list(linear_R)
+activations <- list(sigmoid_R)
 
-out <- simulate_regression_data(rows = 10L, N = 0L, U = 0L, C = numVar, matrices = matrices, activations = activations, noise = 0)
+out <- simulate_classification_data(rows = 10L, N = 0L, U = 0L, C = numVar, matrices = matrices, activations = activations)
 
 test_that("1 normal variable", {
   expect_true(ncol(out) == numVar + 1)
@@ -127,9 +127,9 @@ test_that("1 normal variable", {
 
 numVar <- 5L
 matrices <- list(matrix(1:numVar, nrow = numVar, ncol = 1))
-activations <- list(linear_R)
+activations <- list(sigmoid_R)
 
-out <- simulate_regression_data(rows = 10L, N = 0L, U = 0L, C = numVar, matrices = matrices, activations = activations, noise = 0)
+out <- simulate_classification_data(rows = 10L, N = 0L, U = 0L, C = numVar, matrices = matrices, activations = activations)
 
 test_that("1 normal variable", {
   expect_true(ncol(out) == numVar + 1)
@@ -137,50 +137,35 @@ test_that("1 normal variable", {
 
 rm(numVar, matrices, activations)
 
-###################
-# Argument noise works as expected
-###################
-
-matrices <- list(matrix(1, nrow = 1, ncol = 1))
-activations <- list(linear_R)
-
-out <- simulate_regression_data(rows = 100L, N = 1L, U = 0L, C = 0L, matrices = matrices, activations = activations, noise = 0)
-out2 <- simulate_regression_data(rows = 100L, N = 1L, U = 0L, C = 0L, matrices = matrices, activations = activations, noise = 10)
-test_that("1 normal variable", {
-  expect_true(sd(out[, 2]) < sd(out2[, 2]))
-})
-
-rm(matrices, activations)
-
 ###############################################
 # Input checking
 ###############################################
 
 matrices <- list(matrix(1, nrow = 1, ncol = 1))
-activations <- list(linear_R)
+activations <- list(sigmoid_R)
 
 test_that("Confirm rows checking works.", {
   expect_error(
-    simulate_regression_data(
+    simulate_classification_data(
       rows = c(100, 200),
       N = 1L, U = 0L, C = 0L,
-      matrices = matrices, activations = activations, noise = 0
+      matrices = matrices, activations = activations
     ),
     NULL
   )
   expect_error(
-    simulate_regression_data(
+    simulate_classification_data(
       rows = 100,
       N = 1L, U = 0L, C = 0L,
-      matrices = matrices, activations = activations, noise = 0
+      matrices = matrices, activations = activations
     ),
     NULL
   )
   expect_error(
-    simulate_regression_data(
+    simulate_classification_data(
       rows = -10L,
       N = 1L, U = 0L, C = 0L,
-      matrices = matrices, activations = activations, noise = 0
+      matrices = matrices, activations = activations
     ),
     NULL
   )
@@ -188,26 +173,26 @@ test_that("Confirm rows checking works.", {
 
 test_that("Confirm N checking works.", {
   expect_error(
-    simulate_regression_data(
+    simulate_classification_data(
       rows = 100L,
       N = c(1L, 2L), U = 0L, C = 0L,
-      matrices = matrices, activations = activations, noise = 0
+      matrices = matrices, activations = activations
     ),
     NULL
   )
   expect_error(
-    simulate_regression_data(
+    simulate_classification_data(
       rows = 100L,
       N = 1, U = 0L, C = 0L,
-      matrices = matrices, activations = activations, noise = 0
+      matrices = matrices, activations = activations
     ),
     NULL
   )
   expect_error(
-    simulate_regression_data(
+    simulate_classification_data(
       rows = 100L,
       N = -1L, U = 0L, C = 0L,
-      matrices = matrices, activations = activations, noise = 0
+      matrices = matrices, activations = activations
     ),
     NULL
   )
@@ -215,26 +200,26 @@ test_that("Confirm N checking works.", {
 
 test_that("Confirm U checking works.", {
   expect_error(
-    simulate_regression_data(
+    simulate_classification_data(
       rows = 100L,
       N = 0L, U = c(1L, 2L), C = 0L,
-      matrices = matrices, activations = activations, noise = 0
+      matrices = matrices, activations = activations
     ),
     NULL
   )
   expect_error(
-    simulate_regression_data(
+    simulate_classification_data(
       rows = 100L,
       N = 0L, U = 1, C = 0L,
-      matrices = matrices, activations = activations, noise = 0
+      matrices = matrices, activations = activations
     ),
     NULL
   )
   expect_error(
-    simulate_regression_data(
+    simulate_classification_data(
       rows = 100L,
       N = 0L, U = -1L, C = 0L,
-      matrices = matrices, activations = activations, noise = 0
+      matrices = matrices, activations = activations
     ),
     NULL
   )
@@ -242,26 +227,26 @@ test_that("Confirm U checking works.", {
 
 test_that("Confirm C checking works.", {
   expect_error(
-    simulate_regression_data(
+    simulate_classification_data(
       rows = 100L,
       N = 0L, U = 0L, C = c(1L, 2L),
-      matrices = matrices, activations = activations, noise = 0
+      matrices = matrices, activations = activations
     ),
     NULL
   )
   expect_error(
-    simulate_regression_data(
+    simulate_classification_data(
       rows = 100L,
       N = 0L, U = 0L, C = 1,
-      matrices = matrices, activations = activations, noise = 0
+      matrices = matrices, activations = activations
     ),
     NULL
   )
   expect_error(
-    simulate_regression_data(
+    simulate_classification_data(
       rows = 100L,
       N = 0L, U = 0L, C = -1L,
-      matrices = matrices, activations = activations, noise = 0
+      matrices = matrices, activations = activations
     ),
     NULL
   )
@@ -269,39 +254,38 @@ test_that("Confirm C checking works.", {
 
 test_that("Confirm matrices checking works.", {
   expect_error(
-    simulate_regression_data(
+    simulate_classification_data(
       rows = 100L,
       N = 1L, U = 0L, C = 0L,
       matrices = list(),
-      activations = activations, noise = 0
+      activations = activations
     ),
     NULL
   )
   expect_error(
-    simulate_regression_data(
+    simulate_classification_data(
       rows = 100L,
       N = 1L, U = 0L, C = 0L,
       matrices = list(matrix(1, nrow = 1, ncol = 1), matrix(1, nrow = 1, ncol = 1)),
-      activations = activations,
-      noise = 0
+      activations = activations
     ),
     NULL
   )
   expect_error(
-    simulate_regression_data(
+    simulate_classification_data(
       rows = 100L,
       N = 1L, U = 0L, C = 0L,
       matrices = list(matrix(1:3, nrow = 3, ncol = 1)),
-      activations = activations, noise = 0
+      activations = activations
     ),
     NULL
   )
   expect_error(
-    simulate_regression_data(
+    simulate_classification_data(
       rows = 100L,
       N = 3L, U = 0L, C = 0L,
       matrices = list(matrix(1:3, nrow = 3, ncol = 2)),
-      activations = activations, noise = 0
+      activations = activations
     ),
     NULL
   )
@@ -309,20 +293,29 @@ test_that("Confirm matrices checking works.", {
 
 test_that("Confirm activations checking works.", {
   expect_error(
-    simulate_regression_data(
+    simulate_classification_data(
       rows = 100L,
       N = 1L, U = 0L, C = 0L,
       matrices = matrices,
-      activations = list(), noise = 0
+      activations = list()
     ),
     NULL
   )
   expect_error(
-    simulate_regression_data(
+    simulate_classification_data(
       rows = 100L,
       N = 1L, U = 0L, C = 0L,
       matrices = matrices,
-      activations = list(linear_R, linear_R), noise = 0
+      activations = list(sigmoid_R, sigmoid_R)
+    ),
+    NULL
+  )
+  expect_error(
+    simulate_classification_data(
+      rows = 100L,
+      N = 1L, U = 0L, C = 0L,
+      matrices = matrices,
+      activations = list(exponential_R)
     ),
     NULL
   )
